daa41d52b6bbdd83514c7f902b5f5b9182f4e60d
Haruaki Tamada <tamada@users.noreply.github.com>
2026-07-02 14:53:17 +0900

    feat: Add bash, elvish, fish, powershell and zsh completions for fauxrest

---
+++ assets/completions/elvish/fauxrest.elv
@@ -0,0 +1,39 @@
+
+use builtin;
+use str;
+
+set edit:completion:arg-completer[fauxrest] = {|@words|
+    fn spaces {|n|
+        builtin:repeat $n ' ' | str:join ''
+    }
+    fn cand {|text desc|
+        edit:complex-candidate $text &display=$text' '(spaces (- 14 (wcswidth $text)))$desc
+    }
+    var command = 'fauxrest'
+    for word $words[1..-1] {
+        if (str:has-prefix $word '-') {
+            break
+        }
+        set command = $command';'$word
+    }
+    var completions = [
+        &'fauxrest'= {
+            cand -L 'Specify the log level'
+            cand --level 'Specify the log level'
+            cand -c 'Path to the configuration file'
+            cand --config 'Path to the configuration file'
+            cand -l 'Layout to use for the output'
+            cand --layout 'Layout to use for the output'
+            cand -d 'Path to the output directory'
+            cand --dest 'Path to the output directory'
+            cand -s 'Serializer to use for the output. [available: json, typescript, sql]'
+            cand --serializer 'Serializer to use for the output. [available: json, typescript, sql]'
+            cand --minify 'If true, minify the output'
+            cand -h 'Print help (see more with ''--help'')'
+            cand --help 'Print help (see more with ''--help'')'
+            cand -V 'Print version'
+            cand --version 'Print version'
+        }
+    ]
+    $completions[$command]
+}
---
+++ assets/completions/powershell/fauxrest.ps1
@@ -0,0 +1,45 @@
+
+using namespace System.Management.Automation
+using namespace System.Management.Automation.Language
+
+Register-ArgumentCompleter -Native -CommandName 'fauxrest' -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+
+    $commandElements = $commandAst.CommandElements
+    $command = @(
+        'fauxrest'
+        for ($i = 1; $i -lt $commandElements.Count; $i++) {
+            $element = $commandElements[$i]
+            if ($element -isnot [StringConstantExpressionAst] -or
+                $element.StringConstantType -ne [StringConstantType]::BareWord -or
+                $element.Value.StartsWith('-') -or
+                $element.Value -eq $wordToComplete) {
+                break
+        }
+        $element.Value
+    }) -join ';'
+
+    $completions = @(switch ($command) {
+        'fauxrest' {
+            [CompletionResult]::new('-L', '-L ', [CompletionResultType]::ParameterName, 'Specify the log level')
+            [CompletionResult]::new('--level', '--level', [CompletionResultType]::ParameterName, 'Specify the log level')
+            [CompletionResult]::new('-c', '-c', [CompletionResultType]::ParameterName, 'Path to the configuration file')
+            [CompletionResult]::new('--config', '--config', [CompletionResultType]::ParameterName, 'Path to the configuration file')
+            [CompletionResult]::new('-l', '-l', [CompletionResultType]::ParameterName, 'Layout to use for the output')
+            [CompletionResult]::new('--layout', '--layout', [CompletionResultType]::ParameterName, 'Layout to use for the output')
+            [CompletionResult]::new('-d', '-d', [CompletionResultType]::ParameterName, 'Path to the output directory')
+            [CompletionResult]::new('--dest', '--dest', [CompletionResultType]::ParameterName, 'Path to the output directory')
+            [CompletionResult]::new('-s', '-s', [CompletionResultType]::ParameterName, 'Serializer to use for the output. [available: json, typescript, sql]')
+            [CompletionResult]::new('--serializer', '--serializer', [CompletionResultType]::ParameterName, 'Serializer to use for the output. [available: json, typescript, sql]')
+            [CompletionResult]::new('--minify', '--minify', [CompletionResultType]::ParameterName, 'If true, minify the output')
+            [CompletionResult]::new('-h', '-h', [CompletionResultType]::ParameterName, 'Print help (see more with ''--help'')')
+            [CompletionResult]::new('--help', '--help', [CompletionResultType]::ParameterName, 'Print help (see more with ''--help'')')
+            [CompletionResult]::new('-V', '-V ', [CompletionResultType]::ParameterName, 'Print version')
+            [CompletionResult]::new('--version', '--version', [CompletionResultType]::ParameterName, 'Print version')
+            break
+        }
+    })
+
+    $completions.Where{ $_.CompletionText -like "$wordToComplete*" } |
+        Sort-Object -Property ListItemText
+}
